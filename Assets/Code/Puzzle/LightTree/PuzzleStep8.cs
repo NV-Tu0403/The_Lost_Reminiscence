fe6c2f3a104e177be40c5ad13c6b5c8675bf6de1
Tan Loc <locnntps41055@gmail.com>
2025-07-13 20:05:33 +0700

    Refactor ForceComplete method in puzzle steps to remove NotImplementedException

--- Assets/Code/Puzzle/LightTree/PuzzleStep8.cs
+++ Assets/Code/Puzzle/LightTree/PuzzleStep8.cs
@@ -54,11 +54,6 @@ namespace Code.Puzzle.LightTree
             ReturnCameraToPlayer(sequence, playerCam, cameraMoveDuration, onComplete, characterCamera);
         }
 
-        public void ForceComplete(bool instant = true)
-        {
-            throw new NotImplementedException();
-        }
-
         private void DemoLightingTree()
         {
             // Demo: Tween Emission cho cây nếu có renderer
@@ -73,5 +68,8 @@ namespace Code.Puzzle.LightTree
                 );
             }
         }
+        
+        // Phương thức này sẽ được gọi khi người chơi muốn hoàn thành bước này ngay lập tức
+        public void ForceComplete(bool instant = true) {}
     }
 }
