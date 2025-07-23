8c50db1d4046eba583273d9467a52ce28b23e120
Kiin Nguyễn <tunv0403@gmail.com>
2025-07-23 14:54:54 +0700

    Upadate Current Task

--- Assets/Tu_Develop/Import/Scripts/FaInterface.cs
+++ Assets/Tu_Develop/Import/Scripts/FaInterface.cs
@@ -2,15 +2,10 @@
 
 public interface FaInterface
 {
-    void UseGuideSignal(Vector3 targetPosition);
-    void UseKnowledgeLight(Vector3 areaCenter);
-    void UseProtectiveAura(GameObject target);
-    void UseLightRadar();
-    void UseEgoLight();
-
+    void UseGuideSignal(); // Tín hiệu dẫn lối                  
+    void UseKnowledgeLight(); // Ánh sáng tri thức
+    void UseProtectiveAura(bool self); // Vần sáng bảo hộ (true = Fa, false = Player)
     void OnPlayerCommand(string command);
-    void UpdateLearning(float deltaTime);
-
+    //void UpdateLearning(float deltaTime);
     bool IsSkillAvailable(string skillName);
-    float GetCooldownRemaining(string skillName);
 }
