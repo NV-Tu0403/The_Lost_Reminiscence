9ed2b53a5eec90a9929738b4484cadb8d393417f
DuckLe <vietlq7721900@gmail.com>
2025-08-12 15:29:40 +0700

    Uodate

--- Assets/_MyGame/Codes/UI/UIPage05.cs
+++ Assets/_MyGame/Codes/UI/UIPage05.cs
@@ -70,7 +70,6 @@ public class UIPage05 : MonoBehaviour
         {
             // Xóa các Save Item cũ
             ClearSaveSlots();
-            Core.Instance.backendSync.OnDownload();
 
             // Lấy danh sách SaveFolder từ SaveGameManager thông qua ProfessionalSkilMenu
             SaveListContext context = ProfessionalSkilMenu.Instance.RefreshSaveList();
