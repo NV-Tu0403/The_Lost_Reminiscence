6e141e5f34c397f7787f1b7bb906235f00b54fdb
Tan Loc <locnntps41055@gmail.com>
2025-08-16 17:48:08 +0700

    Update Guide

--- Assets/_MyGame/Codes/Trigger/TriggerZone.cs
+++ Assets/_MyGame/Codes/Trigger/TriggerZone.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
+using _MyGame.Codes.Guidance;
 
 namespace Code.Trigger
 {
     public abstract class TriggerZone : MonoBehaviour
     {
         [SerializeField] public string eventId;
+        
         protected virtual void DisableZone() => gameObject.SetActive(false);
         protected abstract bool IsValidTrigger(Collider other);
 
         protected virtual void OnTriggerEnter(Collider other)
         {
             if (!IsValidTrigger(other)) return;
+            
+            // Thông báo guidance system về tương tác
+            if (GuidanceManager.Instance != null && !string.IsNullOrEmpty(eventId))
+            {
+                GuidanceManager.Instance.OnPlayerInteraction(eventId);
+            }
+            
             OnTriggered(other);
         }
 
