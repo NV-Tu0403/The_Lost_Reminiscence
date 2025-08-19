3f8b34b46875c03eca3d98bd7ed4e41adb356fb1
DuckLe <vietlq7721900@gmail.com>
2025-08-19 22:16:00 +0700

    fix aniamtion Slider setting

---
+++ Assets/_MyGame/Codes/UI/YsncValueSliderbar.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+[Serializable]
+struct YsncValueSliderbarData
+{
+    public Slider[] targetSlider; // Slider B (nhận giá trị)
+}
+
+public class YsncValueSliderbar : MonoBehaviour
+{
+    [SerializeField] private YsncValueSliderbarData targetSlider;
+
+    private Slider thisSlider; // Slider A (nơi gắn script)
+
+    private void Awake()
+    {
+        thisSlider = GetComponent<Slider>(); // Lấy slider A từ chính GameObject gắn script
+    }
+
+    private void OnEnable()
+    {
+        // Đăng ký sự kiện khi giá trị thay đổi
+        thisSlider.onValueChanged.AddListener(SyncValue);
+    }
+
+    private void OnDisable()
+    {
+        // Hủy đăng ký để tránh memory leak
+        thisSlider.onValueChanged.RemoveListener(SyncValue);
+    }
+
+    private void SyncValue(float value)
+    {
+        // Cập nhật giá trị cho tất cả các slider B
+        foreach (Slider slider in targetSlider.targetSlider)
+        {
+            if (slider != null) // Kiểm tra xem slider có null không
+            {
+                slider.value = value; // Cập nhật giá trị của slider B
+            }
+        }
+    }
+}
